94ff93c66c46c374bc399a58b0a93b5f7ec7dded
HTH016 <90416330+HTH016@users.noreply.github.com>
2024-01-12 16:09:04 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,14 +1,16 @@
-def prime(n):
-    cnt = 0
-    for i in range(1, n+1):
-        if n % i == 0:
-            cnt += 1        
-    return cnt    
-
+def is_prime(n):
+    if n == 1:
+        return False
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+    
 n = int(input())
 num_list = list(map(int, input().split(' ')))
 cnt = 0
 for i in range(len(num_list)):
-    if prime(num_list[i]) == 2:
+    if is_prime(num_list[i]) == True:
         cnt += 1
 print(cnt)
