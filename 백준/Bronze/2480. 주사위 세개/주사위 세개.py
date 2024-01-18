c31cc7e77b48ab23973d20000035da499b06a379
HTH016 <90416330+HTH016@users.noreply.github.com>
2024-01-19 08:43:53 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,14 @@
+a, b, c = map(int, input().split(' '))
+
+if a == b and b == c :
+    print(10000 + 1000 * a)
+elif a == b or a == c : 
+    print(1000 + 100 * a)
+elif b == c : 
+    print(1000 + 100 * b)
+elif a > b and a > c : 
+    print( 100 * a )
+elif b > c and b > a : 
+    print( 100 * b )
+else : 
+    print( 100 * c )
