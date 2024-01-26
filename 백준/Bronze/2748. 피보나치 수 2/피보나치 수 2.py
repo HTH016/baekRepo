841a086f58847cf2134df0485475cedef12671ef
HTH016 <90416330+HTH016@users.noreply.github.com>
2024-01-26 10:43:48 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,9 +1,11 @@
-def fibo_dp_bu(n): 
-    memo = [0] * (n + 1)
-    memo[0], memo[1] = 0, 1
-    for i in range(2, n + 1):
-        memo[i] = memo[i - 1] + memo[i - 2]
+memo = [0] * 100
+def fibo_dp_td(n):
+    if n == 1 or n == 2 :
+        return 1
+    if memo[n] != 0:
+        return memo[n] 
+    memo[n] = fibo_dp_td(n - 1) + fibo_dp_td(n - 2)
     return memo[n]
 
 n = int(input())
-print(fibo_dp_bu(n))
+print(fibo_dp_td(n))
