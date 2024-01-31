9354c5671907cb9b2783e79a9ac5d2fa194027cf
HTH016 <90416330+HTH016@users.noreply.github.com>
2024-01-31 22:15:17 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,9 @@
+def tile_2xn(n): 
+    memo = [0] * (n + 1)
+    memo[0], memo[1] = 0, 1
+    for i in range(2, n + 1):
+        memo[i] = memo[i - 1] + memo[i - 2]
+    return memo[n]
+
+n = int(input())
+print(tile_2xn(n+1) % 10007)
